74573789ce083405f4d7f4899d93f0b14465c727
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-22 12:41:42 -0400

    Finish mapVC

--- Experiences/Experiences/View Controllers/MapViewController.swift
+++ Experiences/Experiences/View Controllers/MapViewController.swift
@@ -9,17 +9,76 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
-    
+    let locationHelper = LocationHelper()
+    var experienceController: ExperienceController?
+    var experience: [Experience] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addAnnotation()
+        
+        mapView.delegate = self
+        
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: "ExperienceAnnotationView")
+        
+        //  mapView.showsUserLocation = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        addAnnotation()
         
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        zoomToUsersLocation()
+    }
+    
+    func addAnnotation() {
+        
+        mapView.removeAnnotations(mapView.annotations)
+        guard let experience = experienceController?.experiences else {return}
+        mapView.addAnnotations(experience)
+        
+        let myPin = MKPointAnnotation()
+        if let myCoordinate = experienceController?.newExperiences?.coordinate {
+            myPin.coordinate = myCoordinate
+        }
+        if let annotationText = experienceController?.newExperiences?.postTitle {
+            myPin.title = annotationText
+        }
+        mapView.addAnnotation(myPin)
+    }
+    
+    func zoomToUsersLocation() {
+        //Zoom to user location
+        if let location = locationHelper.currentLocation?.coordinate {
+            let viewRegion = MKCoordinateRegion(center: location, latitudinalMeters: 11500, longitudinalMeters: 11500)
+            mapView.setRegion(viewRegion, animated: true)
+        }
+    }
+    
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard let experience = annotation as? Experience else {return nil}
+        
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "ExperienceAnnotationView", for: experience) as? MKMarkerAnnotationView
+      
+        //annotationView?.glyphText = experience.postTitle
+        annotationView?.glyphTintColor = .white
+        //annotationView?.markerTintColor = .blue
+        annotationView?.titleVisibility = .visible
+        
+        return annotationView
+        
+    }
 
 }
 
